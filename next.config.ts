6fc408dd935e8da7229c3fbfc670e2962e2c4acb
Ashutosh Rath <ashutoshrath1612@gmail.com>
2026-02-16 03:51:37 +0530

    fix public path

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,35 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: isProd ? "/Valentine" : "",
+  assetPrefix: isProd ? "/Valentine/" : "",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
     ],
   },
